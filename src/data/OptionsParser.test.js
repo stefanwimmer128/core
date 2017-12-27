6bfaa05a6abdea5b6b2c11be5352ad8d46889870
stefanwimmer128 <info@stefanwimmer128.eu>
2017-12-27 08:54:08 +0100

    add additional tests for subparsers

--- src/data/OptionsParser.test.js
+++ src/data/OptionsParser.test.js
@@ -48,4 +48,48 @@ describe("data/OptionsParser.js", () => {
             });
         });
     });
+    
+    it("subparser: {}", () => {
+        const opts = new OptionsParser();
+        opts.addOption("subparser", new OptionsParser());
+        assert.deepEqual(opts.parse(), {
+            subparser: {},
+        });
+    });
+    
+    it("subparser: string -> string", () => {
+        const opts = new OptionsParser();
+        opts.addOption("subparser", new OptionsParser().addOption("test", new OptionParser(DataType.STRING)));
+        assert.strictEqual(opts.parse({
+            subparser: {
+                test: "",
+            },
+        }).subparser.test, "");
+    });
+    
+    it("subparser: number -> string", () => {
+        const opts = new OptionsParser();
+        opts.addOption("subparser", new OptionsParser().addOption("test", new OptionParser(DataType.STRING)));
+        assert.throws(() => {
+            opts.parse({
+                subparser: {
+                    test: 0,
+                },
+            });
+        });
+    });
+    
+    it("subparser: string -> string", () => {
+        const opts = new OptionsParser();
+        opts.addOption("subparser", new OptionsParser().addOption("test", new OptionParser(DataType.STRING, "")));
+        assert.strictEqual(opts.parse().subparser.test, "");
+    });
+    
+    it("subparser: number -> string", () => {
+        const opts = new OptionsParser();
+        opts.addOption("subparser", new OptionsParser().addOption("test", new OptionParser(DataType.STRING, 0)));
+        assert.throws(() => {
+            opts.parse();
+        });
+    });
 });
