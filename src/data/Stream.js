445820d2a7c8bea3509c1a675fa550ef1522407e
stefanwimmer128 <info@stefanwimmer128.eu>
2018-01-22 21:56:05 +0100

    add Stream

---
+++ src/data/Stream.js
@@ -0,0 +1,32 @@
+/* @flow */
+
+import createPrivate from "../class/createPrivate";
+
+const $parent = createPrivate("parent");
+const $transformer = createPrivate("transformer");
+
+export default class Stream {
+    static $parent = $parent;
+    static $transformer = $transformer;
+    
+    constructor(parent?: Stream) {
+        $parent(this, parent);
+        
+        $transformer(this, value => value);
+    }
+    
+    pipe(transformer: (value: any) => any): Stream {
+        const stream = new Stream(this);
+        
+        $transformer(stream, transformer);
+        
+        return stream;
+    }
+    
+    value(value: any): any {
+        let val = value;
+        if ($parent(this) instanceof Stream)
+            val = $parent(this).value(val);
+        return $transformer(this)(val);
+    }
+}
